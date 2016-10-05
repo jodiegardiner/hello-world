0fda6236f6cab5ff11e0b854442415b80666a683
Jodie Gardiner <jodiegardiner@outlook.com>
2016-10-05 16:16:33 +0100

    added some more inputs

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 print "Hello, World!"
 
 name = raw_input("Enter your name:- ")
+age = raw_input("How old are you? :- ")
 
-print "Hello, " + name + "!"
+print "Hello, " + name + "! You look great for " + age
